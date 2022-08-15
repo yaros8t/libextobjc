069fff60aeeaee7ab7f205dbf8f825bcf9a336ae
Yaroslav T <yaros8t@gmail.com>
2022-08-15 18:37:10 +0200

    Temp

---
+++ libextobjc/libextobjc.h
@@ -0,0 +1,18 @@
+//
+//  libextobjc.h
+//  libextobjc
+//
+//  Created by Yaroslav Tytarenko on 15.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libextobjc.
+FOUNDATION_EXPORT double libextobjcVersionNumber;
+
+//! Project version string for libextobjc.
+FOUNDATION_EXPORT const unsigned char libextobjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libextobjc/PublicHeader.h>
+
+
